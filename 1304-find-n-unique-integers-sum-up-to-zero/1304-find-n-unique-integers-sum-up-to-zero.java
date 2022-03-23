bd1aaf86654d443179973888f72a1913bd47f014
Deepam Shah <deepam25498@gmail.com>
2022-03-22 19:50:37 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (49.41%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] sumZero(int n) {
         int[] yolo = new int[n];
-        if(n%2==0){
-            for(int i =0;i<n/2;i++){
-                yolo[i] = -n/2+i;
-                yolo[n-1-i] = n/2-i;
-            }
-        }else{
-            for(int i =0;i<n/2+1;i++){
-                yolo[i] = -n/2+i;
-                yolo[n-i-1] = n/2-i;
-            }
+        for(int i =0;i<n/2;i++){
+            yolo[i] = -n/2+i;
+            yolo[n-1-i] = n/2-i;
+        }
+        if(n%2!=0){
             yolo[(n)/2]=0;
         }
         return yolo;
